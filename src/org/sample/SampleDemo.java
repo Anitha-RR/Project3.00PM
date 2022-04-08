2b962d34b5f91ef6e96057da93d8323c0633272c
Anitha-RR <dineshdnesh052@gmail.com>
2022-04-08 15:46:38 +0530

    SampleTwo Added

--- src/org/sample/SampleDemo.java
+++ src/org/sample/SampleDemo.java
@@ -1,10 +1,14 @@
 package org.sample;
 
 public class SampleDemo {
-	
+
 	public void SampleOne() {
-		System.out.println("Sample Project");
+		System.out.println("Sample Project 1");
 
 	}
 
+	private void SampleTwo() {
+		System.out.println("Sample Project 2");
+
+	}
 }
